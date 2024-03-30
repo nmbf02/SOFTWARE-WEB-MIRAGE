0e790ffb0cfd43b6174fd19f6de8f8a2d4a73938
nmbf02 <nathalyberroaf@gmail.com>
2024-03-29 21:39:15 -0400

    Configuración de la ruta de la consulta de la factura
    Creación de la ruta y el controlador.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\consultarFacturaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/consultar-factura/{numerofactura?}', [consultarFacturaController::class, 'consultarFactura'])
+     ->middleware(['auth'])
+     ->name('consultar-factura');
