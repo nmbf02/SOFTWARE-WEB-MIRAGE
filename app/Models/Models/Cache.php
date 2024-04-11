01a4cd6484b951dd7532dab2b0257fdf50052fa9
nmbf02 <nathalyberroaf@gmail.com>
2024-04-11 14:40:35 -0400

    Generación de models automáticos mediante una clase en PHP

---
+++ app/Models/Models/Cache.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cache extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Models/Detalleinventario.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Detalleinventario extends Model
+{
+    use HasFactory;
+}
